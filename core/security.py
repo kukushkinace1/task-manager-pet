b1f9270fed39bbc547ae5e8ee95e2d25c840ee20
ANTON <1999anton36@gmail.com>
2026-01-17 17:41:19 +0300

    .env

--- core/security.py
+++ core/security.py
@@ -1,11 +1,19 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
 import jwt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
+)
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = "CHANGE_ME_SECRET"  # потом вынесем в env
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
 
 
 def get_password_hash(password: str) -> str:
